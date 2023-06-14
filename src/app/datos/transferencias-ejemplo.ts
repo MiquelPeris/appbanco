7e121beadf8260185d0ad7a74940c2bb3242fb37
MiquelPeris <mike94soulfire@gmail.com>
2023-06-14 13:23:44 +0200

    Created pagina transferencias

---
+++ src/app/datos/transferencias-ejemplo.ts
@@ -0,0 +1,26 @@
+export const transferenciasRecibidas = [
+    {
+        id: 1,
+        idOrdenante: 1,
+        idBeneficiario: 2,
+        importe: 50.00,
+        concepto: "Regalo de cumpleaños uwu",
+        fecha: new Date(),
+    },
+    {
+        id: 2,
+        idOrdenante: 2,
+        idBeneficiario: 1,
+        importe: 10.00,
+        concepto: "Cena en X restaurante",
+        fecha: new Date(),
+    },
+    {
+        id: 3,
+        idOrdenante: 1,
+        idBeneficiario: 2,
+        importe: 300.00,
+        concepto: "Habitación de hotel en Honolulu",
+        fecha: new Date(),
+    },
+]
